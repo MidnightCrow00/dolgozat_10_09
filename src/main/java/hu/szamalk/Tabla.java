432881f10937ccfddc1ec3cdba529e7f284910cd
midnightcrow00 <orsolyaalbert1@gmail.com>
2024-12-09 09:35:26 +0100

    f1 4

--- src/main/java/hu/szamalk/Tabla.java
+++ src/main/java/hu/szamalk/Tabla.java
@@ -1,15 +1,47 @@
 package hu.szamalk;
 
+import java.util.Objects;
+
 public class Tabla {
-    private char [][] T;
+    private String [][] T;
     private char UresCella;
 
     public Tabla() {
-        T = new char[8][8];
+        T = new String[][];
     }
 
-    public Tabla(char[][] t, char uresCella) {
-        T = new char[UresCella][UresCella];
-        UresCella = uresCella;
+    public Tabla(char uresCella) {
+        elhelyez(UresCella);
+    }
+
+    public void elhelyez(char uresCella){
+        switch (uresCella) {
+            case 'Ures':
+                T = new String[][]{
+                        {"*", "*", "*", "*","*", "*", "*", "*"},
+                        {"*", "*", "*", "*","*", "*", "*", "*"},
+                        {"*", "*", "*", "*","*", "*", "*", "*"},
+                        {"*", "*", "*", "*","*", "*", "*", "*"},
+                        {"*", "*", "*", "*","*", "*", "*", "*"},
+                        {"*", "*", "*", "*","*", "*", "*", "*"},
+                        {"*", "*", "*", "*","*", "*", "*", "*"},
+                        {"*", "*", "*", "*","*", "*", "*", "*"}
+                };
+            break;
+            default:
+                T = new String[][]{{" "}};
+        }
+    }
+    public void megjelentit(char jel){
+        for (int i = 0; i < T.length; i++) {
+            for (int j = 0; j < T[i].length; j++) {
+                if (T[i][j].equals("*")){
+                    System.out.println(jel);
+                }else {
+                    System.out.println(" ");
+                }
+            }
+            System.out.println();
+        }
     }
 }
