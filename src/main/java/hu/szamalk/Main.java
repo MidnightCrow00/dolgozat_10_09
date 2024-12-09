a6beba6d5b1151e2974ad3a974f68f8adfbda59a
midnightcrow00 <orsolyaalbert1@gmail.com>
2024-12-09 10:12:57 +0100

    f1 7

--- src/main/java/hu/szamalk/Main.java
+++ src/main/java/hu/szamalk/Main.java
@@ -2,6 +2,12 @@ package hu.szamalk;
 
 public class Main {
     public static void main(String[] args) {
-
+        Tabla t = new Tabla('K');
+        for (int i = 0; i < 2; i++) {
+            t.megjelenit('K');
+            System.out.println(" ");
+        }
+        System.out.println("Üres oszlopok száma:"+t.getUresOszlop());
+        System.out.println("Üres sorok száma:"+t.getUresSor());
     }
 }
--- src/main/java/hu/szamalk/Tabla.java
+++ src/main/java/hu/szamalk/Tabla.java
@@ -1,47 +1,92 @@
 package hu.szamalk;
 
+import java.util.Arrays;
 import java.util.Objects;
 
 public class Tabla {
-    private String [][] T;
+    private char T [][];
     private char UresCella;
 
-    public Tabla() {
-        T = new String[][];
+    public Tabla(char[][] T) {
+        this.T = new char[8][8];
     }
 
     public Tabla(char uresCella) {
-        elhelyez(UresCella);
+        elhelyez(uresCella);
     }
 
-    public void elhelyez(char uresCella){
-        switch (uresCella) {
-            case 'Ures':
-                T = new String[][]{
-                        {"*", "*", "*", "*","*", "*", "*", "*"},
-                        {"*", "*", "*", "*","*", "*", "*", "*"},
-                        {"*", "*", "*", "*","*", "*", "*", "*"},
-                        {"*", "*", "*", "*","*", "*", "*", "*"},
-                        {"*", "*", "*", "*","*", "*", "*", "*"},
-                        {"*", "*", "*", "*","*", "*", "*", "*"},
-                        {"*", "*", "*", "*","*", "*", "*", "*"},
-                        {"*", "*", "*", "*","*", "*", "*", "*"}
+    public void elhelyez(char N){
+        switch (N){
+            case 'K':
+                T = new char[][]{
+                        {'*','*','*','*','*','*','*',' '},
+                        {'*',' ',' ',' ',' ',' ',' ',' '},
+                        {' ',' ','*',' ',' ',' ',' ',' '},
+                        {' ',' ',' ',' ',' ',' ',' ',' '},
+                        {' ',' ','*',' ',' ',' ',' ',' '},
+                        {' ',' ','*',' ',' ',' ',' ',' '},
+                        {' ',' ',' ',' ',' ',' ',' ',' '},
+                        {' ',' ',' ',' ',' ',' ',' ',' '}
                 };
-            break;
+                break;
             default:
-                T = new String[][]{{" "}};
+                T = new char[][]{{' '}};
         }
     }
-    public void megjelentit(char jel){
+    public void megjelenit(char jel){
         for (int i = 0; i < T.length; i++) {
             for (int j = 0; j < T[i].length; j++) {
-                if (T[i][j].equals("*")){
-                    System.out.println(jel);
-                }else {
-                    System.out.println(" ");
+                if (Objects.equals(T[i][j], "*")) {
+                    System.out.print(jel);
+                } else {
+                    System.out.print(" ");
                 }
             }
             System.out.println();
         }
+        }
+
+public int getUresSor() {
+    int össz = 0;
+    for (char[] sor : T) {
+        boolean ures = true;
+        for (char elem : sor) {
+            if (Objects.equals(elem, "*")) {
+                ures = false;
+                break;
+            }
+        }
+        if (ures) {
+            össz++;
+        }
+    }
+    return össz;
+}
+    public int getUresOszlop(){
+        int össz = 0;
+        int oszlopokSzama = getUresOszlop();
+
+        for (int j = 0; j < oszlopokSzama; j++) {
+            boolean ures = true;
+            for (int i = 0; i < getUresSor(); i++) {
+                if (Objects.equals(T[i][j], "*")) {
+                    ures = false;
+                    break;
+                }
+            }
+            if (ures) {
+                össz++;
+            }
+        }
+        return össz;
+    }
+
+    @Override
+    public String toString() {
+        return "Tabla{" +
+                "T=" + Arrays.toString(T) +
+                ", UresCella=" + UresCella +
+                '}';
     }
 }
+
